5ef4b2cee67b4c70cd647713136a1edf0ef40fc4
4WardThinking <matthew@fourwardcast.com>
2026-01-16 15:44:46 -0600

    Fix links on footer

--- src/config/footer.js
+++ src/config/footer.js
@@ -17,37 +17,20 @@ export const footerSocials = [
 ]
 
 export const footerLists = [
-	//{
-		//title: 'Landing Pages',
-		//items: [
-			//{
-				//title: 'Landing Page 1',
-				//slug: '/landing-pages/landing-1',
-			//},
-			//{
-				//title: 'Landing Page 2',
-				//slug: '/landing-pages/landing-2',
-			//},
-      // {
-			// 	title: 'Landing Page 3',
-			// 	slug: '/landing-pages/landing-3',
-			// },
-		//],
-	//},
 	{
 		title: 'Company',
 		items: [
 			{
 				title: 'About',
-				slug: '/company/about',
+				slug: 'company/about',
 			},
 			{
 				title: 'Blog',
-				slug: '/blog',
+				slug: 'blog',
 			},
 			{
 				title: 'Contact',
-				slug: '/company/contact',
+				slug: 'company/contact',
 			},
 		],
 	},
